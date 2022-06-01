4bec2f8657d44587d2de9458aa9f9a8488fc5f53
Dohui Ko <bluecard722@hufs.ac.kr>
2022-06-01 20:31:07 +0900

    Add: gitignore . about venv

--- global_forest/pages/views.py
+++ global_forest/pages/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 def home(request):
   return render(request, 'index.html') #index.html을 띄워주는 함수
 
+<<<<<<< Updated upstream
 def onboard(request):
   return render(request, 'onboard.html')
 
@@ -45,3 +46,7 @@ def question6(request):
 
 def result(request):
   return render(request, 'result.html')
+=======
+def test(request):
+  return render(request, 'test.html') #test.html을 띄워주는 함수
+>>>>>>> Stashed changes
