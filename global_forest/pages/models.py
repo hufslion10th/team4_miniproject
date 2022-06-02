b6c6b1c6f7f9f9af7ca81913be28987dce7169b0
seo-jio <lio8625@naver.com>
2022-06-03 01:45:55 +0900

    모델에 nickname field 추가

--- global_forest/pages/models.py
+++ global_forest/pages/models.py
@@ -5,6 +5,7 @@ from .choice import *
 class Result(models.Model):
     student_number = models.CharField(max_length=15, null=True)
     major = models.CharField(max_length=15, null=True)
+    nickname = models.CharField(max_length=20, null=True)
     user_type = models.CharField(choices=TYPE_CHOICES, max_length=15, verbose_name="유저타입", null=False)
     question1 = models.CharField(choices=QUESTION1_CHOICES, max_length=15, verbose_name="질문 1", null=False)
     question2 = models.CharField(choices=QUESTION2_CHOICES, max_length=15, verbose_name="질문 2", null=False)
@@ -12,3 +13,6 @@ class Result(models.Model):
     question4 = models.CharField(choices=QUESTION4_CHOICES, max_length=15, verbose_name="질문 4", null=False)
     question5 = models.CharField(choices=QUESTION5_CHOICES, max_length=15, verbose_name="질문 5", null=False)
     question6 = models.CharField(choices=QUESTION6_CHOICES, max_length=15, verbose_name="질문 6", null=False)
+
+    def __str__(self):
+        return self.nickname
