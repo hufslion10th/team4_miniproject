c261865de19a3d20dbf9dd128f307cc01b612944
Dohui Ko <bluecard722@hufs.ac.kr>
2022-06-04 01:56:55 +0900

    Add: btn선택에 따른 로직 구현 & 태그 구현

--- global_forest/static/main.js
+++
@@ -1,7 +0,0 @@
-const q1 = document.querySelector("#question1");
-const q2 = document.querySelector("#question2");
-
-function start() {
-    q1.style.display = "none";
-    q2.style.display = "block";
-}
