e6f5dc511f6f7be471bc31dca16bffd616042448
seo-jio <lio8625@naver.com>
2022-06-05 01:02:20 +0900

    form complete

--- global_forest/pages/views.py
+++ global_forest/pages/views.py
@@ -13,11 +13,12 @@ class ResultCreateView(CreateView):
     form_class = ResultModelForm
 
     def get_success_url(self):
-        return reverse('home')
+        return reverse('result')
 
+    # 결과 값 저장시에만 사용
     # def form_valid(self, form):  #사용자가 입력한 값에 result를 추가해서 db에 save
-    #   form.instance.result = self.request.POST["result"]
-    #   return super().form_valid(form)
+    # form.instance.result = self.request.POST["result"]
+    # return super().form_valid(form)
 
 def result(request):
     return render(request, 'result.html')
